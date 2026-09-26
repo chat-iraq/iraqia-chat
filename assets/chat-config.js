b40f7753b7b70ec91d1de6ef8d37b9a9f466670b
chat-iraq <kaz3asl@gmail.com>
2026-09-26 03:07:34 +0300

    feat: enable live chat on Firebase Realtime DB (asltime-ds-chat-fire) with real Firebase project config

--- assets/chat-config.js
+++ assets/chat-config.js
@@ -1,8 +1,8 @@
 window.CHAT_FIREBASE = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyBZ8X6Wvc0jY7P-9Wc3DybdnqIiqEUoOxA",
+  authDomain: "asltime-ds-chat-fire.firebaseapp.com",
+  databaseURL: "https://asltime-ds-chat-fire-default-rtdb.firebaseio.com",
+  projectId: "asltime-ds-chat-fire",
+  appId: "1:726652877785:web:fee8f5c43c942d095a77c8",
   roomPrefix: "durar"
 };
